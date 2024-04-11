f7d6839161c45826cd9c68add448cb159fe42b9e
NITRO\fardin <fardin467@gmail.com>
2024-04-11 23:26:14 +0600

    update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <img
+      className="avatar"
+      src="https://avatars.githubusercontent.com/u/99902301?v=4"
+      alt="Avatar"
+    />
+  );
+}
+function Intro() {
+  return (
+    <div>
+      <h1>John Doe</h1>
+      <p>Web Developer</p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skills">
+      <Skill name="HTML" level="90%" />
+      <Skill name="CSS" level="80%" />
+      <Skill name="JavaScript" level="70%" />
+      <Skill name="React" level="60%" />
+    </div>
+  );
+}
+
+function Skill({ name, level }) {
+  return (
+    <div className="skill">
+      <h3>{name}</h3>
+      <div className="level" style={{ width: level }}>
+        {level}
+      </div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
